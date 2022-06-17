1078719fc354f56f8e0735bc677ec4248cdb0bfd
7x7x49 <98600734+7x7x49@users.noreply.github.com>
2022-06-17 13:31:45 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
-interface Observable {                //всем переданным ему Observer-объектам сообщит о том, что событие наступило.
+interface Observable {                //издатель всем переданным ему Observer-объектам (подписчикам) сообщит о том, что событие наступило.
     void notifyObservers();           //При получении нового сообщения отправляется оповещение всем наблюдателям
     void regObserver(Observer o);
 }
